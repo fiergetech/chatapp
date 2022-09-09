af0df92365e7b49229f0622a5d2c385c18d4d6ad
Hindro <hindrowahid@gmail.com>
2022-09-09 10:45:15 +0700

    google auth

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,14 +1,51 @@
 import 'package:chatapp/app/routes/app_pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthController extends GetxController {
   var isSkipIntro = false.obs;
   var isAuth = false.obs;
-  void login() {
-    Get.offAllNamed(Routes.HOME);
+  UserCredential? userCredential;
+
+  GoogleSignInAccount? _currentUser;
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  void firstInitialized() {}
+
+  Future<void> login() async {
+    try {
+      await _googleSignIn.signOut();
+      await _googleSignIn.signIn().then((value) => _currentUser = value);
+      final isSignIn = await _googleSignIn.isSignedIn();
+
+      if (isSignIn) {
+        // to handle user data leak before login
+        print("Success login with : ");
+        print(_currentUser);
+        final googleAuth = await _currentUser!.authentication;
+        final credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        );
+
+        await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .then((value) => userCredential = value);
+        print("User credential");
+        print(userCredential);
+        isAuth.value = true;
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        print("Login failed");
+      }
+    } catch (error) {
+      printError();
+    }
   }
 
-  void logout() {
+  Future<void> logout() async {
+    await _googleSignIn.signOut();
     Get.offAllNamed(Routes.LOGIN);
   }
 }
