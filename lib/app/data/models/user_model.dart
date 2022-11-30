f0b7644679d15b56fd3b7f62ce3ae917eef4fdc0
fiergetech <hindrowahid@gmail.com>
2022-11-30 23:17:09 +0700

    user model

---
+++ lib/app/data/models/user_model.dart
@@ -0,0 +1,44 @@
+class User {
+  String? uid;
+  String? name;
+  String? email;
+  String? createdAt;
+  String? lastSignInTime;
+  String? photoUrl;
+  String? status;
+  String? updatedAt;
+
+  User(
+      {this.uid,
+      this.name,
+      this.email,
+      this.createdAt,
+      this.lastSignInTime,
+      this.photoUrl,
+      this.status,
+      this.updatedAt});
+
+  User.fromJson(Map<String, dynamic> json) {
+    uid = json['uid'];
+    name = json['name'];
+    email = json['email'];
+    createdAt = json['createdAt'];
+    lastSignInTime = json['lastSignInTime'];
+    photoUrl = json['photoUrl'];
+    status = json['status'];
+    updatedAt = json['updatedAt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['uid'] = uid;
+    data['name'] = name;
+    data['email'] = email;
+    data['createdAt'] = createdAt;
+    data['lastSignInTime'] = lastSignInTime;
+    data['photoUrl'] = photoUrl;
+    data['status'] = status;
+    data['updatedAt'] = updatedAt;
+    return data;
+  }
+}
