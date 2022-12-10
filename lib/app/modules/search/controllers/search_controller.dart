7d788f70b8e64487608a3f6ce171d1764256a360
fiergetech <hindrowahid@gmail.com>
2022-12-10 14:50:22 +0700

    search view

--- lib/app/modules/search/controllers/search_controller.dart
+++ lib/app/modules/search/controllers/search_controller.dart
@@ -1,8 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SearchController extends GetxController {
   late TextEditingController searchC;
+
+  var queryFirst = [].obs;
+  var tempSearch = [].obs;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  void searchUser(String data) async {
+    print("SEARCH  : $data");
+    if (data.length == 0) {
+      queryFirst.value = [];
+      tempSearch.value = [];
+    } else {
+      var capitalized = data.substring(0, 1).toUpperCase() + data.substring(1);
+      print(capitalized);
+      if (queryFirst.length == 0 && data.length == 1) {
+        //first function to start while first type
+        CollectionReference clients = await firestore.collection("clients");
+        final keyNameResult = await clients
+            .where("keyName", isEqualTo: data.substring(0, 1).toUpperCase())
+            .get();
+        print("TOTAL DATA: ${keyNameResult.docs.length}");
+        if (keyNameResult.docs.length > 0) {
+          for (int i = 0; i < keyNameResult.docs.length; i++) {
+            queryFirst
+                .add(keyNameResult.docs[i].data() as Map<String, dynamic>);
+          }
+          print("QUERY RESULT : ");
+          print(queryFirst);
+        } else {
+          print("NO DATA FOUND");
+        }
+      }
+      //String name = "Klugaga Singa";
+      //name.startsWith(element)
+      if (queryFirst.length != 0) {
+        tempSearch.value = [];
+        queryFirst.forEach((element) {
+          if (element["name"].startsWith(capitalized)) {
+            tempSearch.add(element);
+          }
+        });
+      }
+    }
+    queryFirst.refresh();
+    tempSearch.refresh();
+  }
+
   @override
   void onInit() {
     searchC = TextEditingController();
